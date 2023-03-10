cd365a0221f5c4c7c9556ccee2d0d6c9eedd1df9
Atul <atul.kumar@technogetic.com>
2023-03-10 18:02:31 +0530

    toolkit added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { Provider } from 'react-redux';
+import store from './Redux-toolkit/Store';
 function App() {
   return (
-    <div className="App">
-      Boilerplate code for react,typescript and redux-toolkit
-    </div>
+    <Provider store={store}>
+      <div className="App">Boilerplate code for react,typescript and redux-toolkit</div>
+    </Provider>
   );
 }
 
